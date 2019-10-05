c4a5da8fb54738b8221f9d635267a624e99177b0
T3k5upp0rt <kylan.childers@gmail.com>
2019-10-05 10:13:59 -0700

    fixed apiroutes js file

---
+++ routes/apiRoutes.js
@@ -0,0 +1,22 @@
+var apiRoutes = require('express').Router();
+
+
+apiRoutes.get("/signup", function(req, res) {
+    
+});
+
+apiRoutes.get("/driversignup", function(req, res) {
+    
+});
+
+apiRoutes.get("/signin", function(req, res) {
+    
+});
+
+apiRoutes.get("/driversignin", function(req, res) {
+    
+});
+
+
+
+module.exports = apiRoutes;
